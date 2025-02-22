d1a8536abc1310245060e67f6bde060b31c5aecd
Azamovobidxon <azamovobid733@gmail.com>
2025-02-22 21:55:50 +0300

    sdfs

--- three.dart
+++ three.dart
@@ -1,16 +1,7 @@
 void main() {
-  int a;
-  int b;
-  a = 0;
-  b = ++a;
-  print(a);
-  print(b);
-  int a1;
-  int b1;
+  double numberFloat = 10.0;
 
-  a1 = 5;
-  b1 = a1 ++;
-  print('a $a1');
-  print('b $b1');
+  
+
   
 }
