9826c7c1b62af10eeed200d239ca040f1d80d05c
Azamovobidxon <azamovobid733@gmail.com>
2025-02-25 21:39:13 +0300

    while loop

--- four.dart
+++ four.dart
@@ -1,3 +1,26 @@
+
+
 void main() {
-  print('sdf');
+  int number = 10;
+  int count = 0;
+
+  while (number > count) {
+    count++;
+    print(count);
+  }
+
+  while(number >= 1) {
+    print(number);
+    number--;
+  }
+  int l = 1;
+  int yigindi = 0;
+ 
+  while(l < 25){
+   yigindi += l;
+   l++;
+  }
+  print('1 dan 25 gacha sonlar yig\'indisi $yigindi');
+  
+ 
 }
