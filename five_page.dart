ed7fc23ba6133f19bc69e475b7c8131f465df8dc
Azamovobidxon <azamovobid733@gmail.com>
2025-03-01 22:55:17 +0300

    barcha function qiymat qaytaradi agar qaytish turi ko'rssatilmagan bo'lsa statment null qaytardi

--- five_page.dart
+++ five_page.dart
@@ -1,22 +1,4 @@
 void main() {
-  myInfo('Da  rt', age: 15);
-  String numberString1 = '123';
-  String numberString2 = 'abc';
-  int number = int.parse(numberString1);
-  print(number);
-  int? number2 = int.tryParse(numberString2);
-  print(number2);
-  
+ print(foo() == null);
 }
-
-void myInfo(String name, {required int age}) {
-  print('$name $age');
-}
-
-void myInfo2({String? name, int? age}) {
-  print('$name $age');
-}
-
-int myTryParse(String source, {int? radix = 10}) {
-  return int.parse(source, radix: radix);
-}
+foo(){}
