6ecaf411b45e1a7ac9d0933572f1cec63cd7822f
Azamovobidxon <azamovobid733@gmail.com>
2025-03-01 07:27:53 +0300

    myTryParse Function I own made

--- five_page.dart
+++ five_page.dart
@@ -1,72 +1,22 @@
 void main() {
-  declareFunction('Hello everyone');
-  int firstA = 34;
-  int firsB = 34;
-
-  int result = add(firstA, firsB);
-  print(result);
-
-  print(add(34, 34));
-
-  isOdd(6);
-  sonYigindi(1, 25);
-  print('');
-  print('sumRange, a dan b gacha nonlar yig\'indisi  ${sumRange(1, 25)}');
-  print('');
-  print('ucht soning o\'rta arfimetigi ${middleArfmetig(34, 1, 34)}');
-
-  print('katta son ${maxNumber(1000, 101, 500)}');
+  myInfo('Da  rt', age: 15);
+  String numberString1 = '123';
+  String numberString2 = 'abc';
+  int number = int.parse(numberString1);
+  print(number);
+  int? number2 = int.tryParse(numberString2);
+  print(number2);
+  
 }
 
-void declareFunction(String msg) {
-  print('Massage: $msg');
+void myInfo(String name, {required int age}) {
+  print('$name $age');
 }
 
-int add(int a, int b) {
-  return a + b;
+void myInfo2({String? name, int? age}) {
+  print('$name $age');
 }
 
-void isOdd(int n) {
-  if (n < 0) {
-    print('0 dan katta son kriting');
-  } else if (n.isEven) {
-    print(true);
-  } else {
-    print(false);
-  }
-}
-
-void sonYigindi(int a, int b) {
-  int sum = 0;
-  while (a <= b) {
-    sum += a;
-    a++;
-  }
-  print('a dan b gacha sonlar yig\'indsi $sum');
-}
-
-int sumRange(int a, int b) {
-  int sum = 0;
-  for (int i = a; i <= b; i++) {
-    sum += i;
-  }
-  return sum;
-}
-
-int middleArfmetig(int a, int b, int c) {
-  return (a + b + c) ~/ 3;
-}
-
-int maxNumber(int a, int b, int c) {
-  int sum = 0;
-  if (a > b) {
-    sum = a;
-  } else if (b > c) {
-    sum = b;
-  } else if (c > a) {
-    sum = c;
-  } else {
-    print('nimadir xato');
-  }
-  return sum;
+int myTryParse(String source, {int? radix = 10}) {
+  return int.parse(source, radix: radix);
 }
